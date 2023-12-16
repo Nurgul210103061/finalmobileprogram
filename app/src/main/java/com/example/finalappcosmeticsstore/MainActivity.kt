be5c890ca4130a2acd12594f2e0266f7e7b5af12
Nurgul210103061 <210103061@stu.sdu.edu.kz>
2023-12-17 02:58:49 +0300

    navigationbottom

--- app/src/main/java/com/example/finalappcosmeticsstore/MainActivity.kt
+++ app/src/main/java/com/example/finalappcosmeticsstore/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.finalappcosmeticsstore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.finalappcosmeticsstore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    
+    private lateinit var binding :ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+        binding.bottomNavigationView.setOnItemSelectedListener{
+            when(it.itemId){
+                R.id.home -> {
+                    changeFragment(HomeFragment())
+                }
+            }
+        }
+    }
+    fun changeFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 }
+
