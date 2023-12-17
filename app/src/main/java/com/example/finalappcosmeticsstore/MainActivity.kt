62189a5a5c2c3d8e74fe1a7aca9205e28e1f89f9
Nurgul210103061 <210103061@stu.sdu.edu.kz>
2023-12-17 03:11:53 +0300

    navigationbottom2

--- app/src/main/java/com/example/finalappcosmeticsstore/MainActivity.kt
+++ app/src/main/java/com/example/finalappcosmeticsstore/MainActivity.kt
@@ -12,13 +12,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
+
+        changeFragment(HomeFragment())
         binding.bottomNavigationView.setOnItemSelectedListener{
             when(it.itemId){
                 R.id.home -> {
                     changeFragment(HomeFragment())
                 }
+                R.id.cart ->{
+                    changeFragment(CartFragment())
+                }
+                R.id.search -> {
+                    changeFragment(SearchFragment())
+                }
+                R.id.history -> {
+                    changeFragment(HistoryFragment())
+                }
+                R.id.profile -> {
+                    changeFragment(ProfileFragment())
+                }
             }
+
+            return@setOnItemSelectedListener true
         }
     }
     fun changeFragment(fragment: Fragment) {
